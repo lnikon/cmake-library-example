1c3859d391413da589fe92669626105aa5b64937
lnikon <nreplay@list.ru>
2018-06-10 17:28:46 +0400

    cmake project structure for the shared/static c++ library

---
+++ example/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "yourlib/yourlib.hpp"
+
+int main() {
+    std::cout << "My example works!\n";
+    return 0;
+}
---
+++ include/yourlib/yourlib.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace nukle {
+
+template <class T>
+class Dictionary {
+public:
+    const char* return_cstring()
+    {
+        return "Hello from Lib\n";
+    }
+};
+
+}
